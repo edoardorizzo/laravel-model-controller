0b836dd95f7f0e3d1b988316ac5e919dec51e24c
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-24 15:00:14 +0200

    fix movie path

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use App\Models\Movie;
+use App\Movie;
 
 class PageController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
 use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'index']);
+
